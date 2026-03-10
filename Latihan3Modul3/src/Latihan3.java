ca6eeb56990e6414771821a9bf065d9c1c6567ef
sultanzulvi <akundump27@gmail.com>
2026-03-10 16:59:40 +0700

    PostTest

--- Latihan3Modul3/src/Latihan3.java
+++ Latihan3Modul3/src/Latihan3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Latihan1 {
+public class Latihan3 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -11,7 +11,7 @@ public class Latihan1 {
         System.out.println("Presentasi Absensi : ");
         double absensi = input.nextDouble();
 
-        String hasil = (nilaiAkhir > 75 && absensi > 80) ? "Lulus" : "Tidak Lulus";
+        String hasil = (nilaiAkhir >= 75 && absensi >= 80) ? "Lulus" : "Tidak Lulus";
 
         System.out.println("Hasil Akhir : " + hasil);
 
--- PostTest_Modul3/src/PostTest.java
+++ PostTest_Modul3/src/PostTest.java
@@ -1,5 +1,55 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class PostTest {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int saldo = 500000;
+        int pilih;
+
+        do{
+        System.out.println("===== MENU =====");
+        System.out.println("1. Cek Saldo");
+        System.out.println("2. Setor Tunai");
+        System.out.println("3. Tarik Tunai");
+        System.out.println("4. Exit");
+
+        pilih = input.nextInt();
+
+        switch (pilih){
+            case 1:
+            System.out.print("Saldo : " + saldo);
+            break;
+
+            case 2:
+            System.out.print("Setor tunai : ");
+            int setor = input.nextInt();
+            
+            System.out.print("Saldo : " + ( saldo + setor ));
+            break;
+
+            case 3:
+            System.out.print("Tarik saldo : ");
+            int tarikSaldo = input.nextInt();
+
+            if( saldo - tarikSaldo <= 50000){
+                System.out.println("Sisa saldo anda kurang dari 50000!!");
+            }else{
+                System.out.print("Saldo : " + (saldo - tarikSaldo) );
+            }
+            break;
+
+            case 4:
+                System.out.println("Terima kasih, datang kembali!!");
+                break;
+
+            default:
+                System.out.println("Pilih sesuai menu!!");
+        }
+        } while (pilih != 4);
+        
+        input.close();
+
     }
 }
